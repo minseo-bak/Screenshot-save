2a33657fda961fd7331d7bf4957494cf09e96a2d
minseo-bak <80301986+minseo-bak@users.noreply.github.com>
2024-04-02 11:15:41 +0900

    Add files via upload
    에러가 존재하는 브랜치입니다. 
    현재 스크린샷출력부분 추가
    비트맵 섬네일 출력부분추가했습니다

--- AltSaveScreen_swap.js
+++ AltSaveScreen_swap.js
@@ -54,6 +54,52 @@
 
     };
 
+
+    //아래로 스크린샷 캡쳐하는 스크립트 
+    const _dataManager_saveGame = DataManager.saveGame;
+    DataManager.saveGame = function(savefileId) {
+    const result = _dataManager_saveGame.call(this, savefileId);
+
+    const bitmap = this.makeSavefileBitmap();
+    if (bitmap){
+        const snapUrl = bitmap.toDataURL();
+        this.saveThumbnail(savefileId, snapUrl);
+    }
+
+    return result;
+};
+    //위로 스크린샷 캡쳐하는 스크립트 
+
+    //아래로 스크린샷로드 스크립트
+    const _dataManager_loadAllSavefileImages = DataManager.loadAllSavefileImages;
+    DataManager.loadAllSavefileImages = function() {
+        _dataManager_loadAllSavefileImages.call(this);
+        for (let id = 0; id < DataManager.maxSavefiles(); id++) {
+            this.loadThumbnail(id);
+        }
+    };
+
+    //위로 스크린샷 로드 스크립트 
+
+    //로드섬네일
+
+    DataManager.loadThumbnail = function(id) {
+        if (!this._snapUrls[id]) {
+            const filename = this.makeThumbnailFilename(id);
+            if (StorageManager.exists(filename)) {
+                this._snapUrls[id] = StorageManager.loadObject(filename).then(url => {
+                    this._snapUrls[id] = url;
+                    return 0;
+                });
+            }
+        }
+        return this._snapUrls[id];
+    };
+
+    // 맨아래에하나더잇어 
+
+
+
     const _Scene_File_start = Scene_File.prototype.start;
     Scene_File.prototype.start = function() {
         _Scene_File_start.apply(this, arguments);
@@ -118,20 +164,100 @@
         }
     };
 
+    // Window_SavefileStatus.prototype.drawContents = function(info, rect) {
+    //     const bottom = rect.y + rect.height;
+    //     const playtimeY = bottom - this.lineHeight();
+    //     this.drawText(info.title, rect.x + 192, rect.y, rect.width - 192);
+    //     this.drawPartyfaces(info.faces, rect.x, bottom - 144);
+    //     this.drawText(info.playtime, rect.x, playtimeY, rect.width, "right");
+    //     this.drawSnappedImage();//id, valid, thumbnail_X, thumbnail_Y); //??진심?
+    // };
+
+    
     Window_SavefileStatus.prototype.drawContents = function(info, rect) {
         const bottom = rect.y + rect.height;
         const playtimeY = bottom - this.lineHeight();
         this.drawText(info.title, rect.x + 192, rect.y, rect.width - 192);
-        this.drawPartyfaces(info.faces, rect.x, bottom - 144);
+        //this.drawPartyfaces(info.faces, rect.x, bottom - 144);
         this.drawText(info.playtime, rect.x, playtimeY, rect.width, "right");
+        this.drawSnappedImage();
     };
+    
+    
+ //스크린샷 출력하는곳
+//     Window_SavefileStatus.prototype.drawSnappedImage = function()//savefileId, valid, x, y) 
+//     {
+//     const snapUrl = DataManager.getThumbnail(this._savefileId); //+this._
+//     if (showThumbnail && snapUrl) {
+//         const bitmap = ImageManager.loadThumbnailFromUrl(snapUrl);
+//         this.changePaintOpacity(true);
+//         bitmap.addLoadListener(function() {
+//             //this.contents.blt(bitmap, 0, 0, bitmap.width, bitmap.height, x, y);
+//             this.contents.blt(bitmap, 0, 0, bitmap.width, bitmap.height, thumbnail_X, thumbnail_Y);
+//         }.bind(this));
+//     }
+// };
 
-    Window_SavefileStatus.prototype.drawPartyfaces = function(faces, x, y) {
-        if (faces) {
-            for (let i = 0; i < faces.length; i++) {
-                const data = faces[i];
-                this.drawFace(data[0], data[1], x + i * 150, y);
-            }
+Window_SavefileStatus.prototype.drawSnappedImage = function() {
+    const snapUrl = DataManager.getThumbnail(this._savefileId);
+    if (showThumbnail && snapUrl) {
+        const bitmap = ImageManager.loadThumbnailFromUrl(snapUrl);
+        this.changePaintOpacity(true);
+        bitmap.addLoadListener(function() {
+            this.contents.blt(bitmap, 0, 0, bitmap.width, bitmap.height, thumbnail_X, thumbnail_Y);
+        }.bind(this));
+    }
+};
+//스크린샷 출력이야  
+
+    
+
+// Window_SavefileStatus.prototype.drawPartyfaces = function(faces, x, y) {
+//     if (faces) {
+//         for (let i = 0; i < faces.length; i++) {
+//             const data = faces[i];
+//             this.drawFace(data[0], data[1], x + i * 150, y);
+//         }
+//     }
+// };
+//로드섬네일추가
+
+ImageManager.loadThumbnailFromUrl = function(url) {
+    const cache = url.includes("/system/") ? this._system : this._cache;
+    if (!cache[url]) {
+        cache[url] = Bitmap.loadThumbnail(url);
+    }
+    return cache[url];
+}
+
+Bitmap.loadThumbnail = function(url) {
+    const bitmap = Object.create(Bitmap.prototype);
+    bitmap.initialize();
+    bitmap._url = url;
+    bitmap.isThumbnail = true;
+    bitmap._startLoading();
+    return bitmap;
+};
+
+Bitmap.prototype._startLoading = function() {
+    this._image = new Image();
+    this._image.onload = this._onLoad.bind(this);
+    this._image.onerror = this._onError.bind(this);
+    this._destroyCanvas();
+    this._loadingState = "loading";
+    if (Utils.hasEncryptedImages() && !this.isThumbnail) {
+        this._startDecrypting();
+    } else {
+        this._image.src = this._url;
+        if (this._image.width > 0) {
+            this._image.onload = null;
+            this._onLoad();
         }
-    };
+    }
+};
+
+
+// 로드비트맵섬네일관리끝 
+
+
 })();
